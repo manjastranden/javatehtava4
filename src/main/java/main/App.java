b98293a01d01351d505624305ceea5aeee66b791
manjastranden <manja.stranden@student.lut.fi>
2024-02-05 21:48:18 +0200

    uusi

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -15,7 +15,7 @@ public class App
 
         boolean exit = false;
         while(!exit) {
-            System.out.println("1) Lisää opiskelija, 2) Listaa opiskelijat, 3) Lisää opiskelijalle suorite, 4) Listaa opiskelijan suoritteet, 5) Laske opiskelijan suoritusten keskiarvo, 6) Laske opiskelijan suoritusten mediaani, 7) Tallenna opiskelija tiedostoon, 8) Lataa opiskelijat tiedostosta, 0) Lopeta ohjelma");
+            System.out.println("1) Lisää opiskelija, 2) Listaa opiskelijat, 3) Lisää opiskelijalle suorite, 4) Listaa opiskelijan suoritteet, 5) Laske opiskelijan suoritusten keskiarvo, 6) Laske opiskelijan suoritusten mediaani, 7) Tallenna opiskelijat tiedostoon, 8) Lataa opiskelijat tiedostosta, 0) Lopeta ohjelma");
             
             if(sc.hasNext()) {
                 int i = 0;
@@ -26,7 +26,7 @@ public class App
                     case 1:
                         System.out.println("Anna opiskelijan nimi?");
                         String studentName = sc.nextLine();
-                        System.out.println("Anna opiskelijanumero");
+                        System.out.println("Anna opiskelijan opiskelijanumero:");
                         String studentNumber = sc.nextLine();
                         Student newStudent = new Student(studentName, studentNumber);
                         university.addStudent(newStudent);
